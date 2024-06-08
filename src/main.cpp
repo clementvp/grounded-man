a5b2645e2e125fc7a7ada5b7233ca2b9e1b33b82
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-06-08 22:37:26 +0200

    refactor code (remove annoying sound for now)

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,34 @@ void setup() {
     StickCP2.Display.setRotation(1);
 }
 
+bool flashed = false;
+
 
 void flashScreenAndBeep(void){
-    StickCP2.Display.fillCircle(StickCP2.Display.width() / 2, StickCP2.Display.height() /2 , 50, RED);
-    StickCP2.Speaker.tone(4000, 500);
-    delay(500);
-    StickCP2.Display.clear();
-    StickCP2.Speaker.tone(10000, 500);
-    delay(500);
+        delay(500);
+        StickCP2.Display.fillCircle(StickCP2.Display.width() / 2, StickCP2.Display.height() /2 , 50, RED);
+        delay(500);
+        StickCP2.Display.clear();
 }
 
 void loop(void) {
     auto imu_update = StickCP2.Imu.update();
     if (imu_update) {
         auto data = StickCP2.Imu.getImuData();
-        Serial.printf("ax:%f  ay:%f  az:%f\r\n", data.accel.x, data.accel.y, data.accel.z);
+       
         if(abs(data.accel.y) < 0.60){
+            if(!flashed){
+                Serial.printf("flashed passe a true\n");
+                flashed = true;
+            }
+        }else{
+            Serial.printf("flashed passe a false\n");
+            flashed = false;
+        }
+
+        if(flashed){
             flashScreenAndBeep();
         }
-        
     }
+    delay(100);
 }
