97c5ecf222ab165c3bdca85ad6d7aba1a85be510
Clément Van Peuter <clement.vanpeuter@gmail.com>
2024-06-08 21:37:45 +0200

    Add a flashing screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,16 @@ void setup() {
     auto cfg = M5.config();
     StickCP2.begin(cfg);
     StickCP2.Display.setRotation(1);
-    StickCP2.Display.setBrightness(0);
+}
+
+
+void flashScreenAndBeep(void){
+    StickCP2.Display.fillCircle(StickCP2.Display.width() / 2, StickCP2.Display.height() /2 , 50, RED);
+    StickCP2.Speaker.tone(4000, 500);
+    delay(500);
+    StickCP2.Display.clear();
+    StickCP2.Speaker.tone(10000, 500);
+    delay(500);
 }
 
 void loop(void) {
@@ -12,10 +21,9 @@ void loop(void) {
     if (imu_update) {
         auto data = StickCP2.Imu.getImuData();
         Serial.printf("ax:%f  ay:%f  az:%f\r\n", data.accel.x, data.accel.y, data.accel.z);
-        if(abs(data.accel.y) < 0.50){
-            StickCP2.Speaker.tone(4000, 100);
+        if(abs(data.accel.y) < 0.60){
+            flashScreenAndBeep();
         }
         
     }
-    delay(100);
 }
